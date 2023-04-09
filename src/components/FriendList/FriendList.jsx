3c44d4da6bc2be657273d2b247216899f5ecbd4b
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-04-09 17:59:35 +0300

    css friends & transaction

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
-import FriendListItem from "./FriendListItem/FriendListItem"
+import FriendListItem from "./FriendListItem/FriendListItem";
+import css from './FriendList.module.css'
 
 export default function FriendList({friends}) {
     return (
-        <ul>
+        <ul className={css.friendBox}>
             {friends.map(({id, avatar, name, isOnline,}) => (
                 <FriendListItem
                     avatar={avatar}
