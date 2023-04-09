48e1a358d595bfd438a7a95eae7f500e8f7c9c5c
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-04-09 16:40:09 +0300

    css Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,20 +1,23 @@
 import PropTypes from 'prop-types';
+import css from './Statistics.module.css'
 
-// function randomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+function randomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 export default function Statistics({title, stats}) {
     return (
-        <section class="statistics">
-            <h2 class="title">{title}</h2>
+        <section className={css.statistics}>
+            {title && <h2 className={css.title}>{title}</h2>}
 
-            <ul class="stat-list">
+            <ul className={css.statList}>
                 {stats.map(({ id, label, percentage }) => {
                     return (
-                        <li class="item" key={id}>
-                            <span class="label">{label}</span>
-                            <span class="percentage">{percentage} %</span>
+                        <li className={css.item}
+                            key={id}
+                            style={{backgroundColor: randomHexColor()}}>
+                            <span className={css.label}>{label}</span>
+                            <span className={css.percentage}>{percentage} %</span>
                         </li>
                     );
                 })}
