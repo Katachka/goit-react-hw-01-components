c1ee2a3558d8543d8b06f07c1b90a6e112730712
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-04-09 15:13:27 +0300

    css profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,11 +9,11 @@ import user from "data/user.json";
 import data from "data/data.json";
 import friends from "data/friends.json";
 import transactions from "data/transactions.json";
-
+import css from "components/App.module.css"
 
 export const App = () => {
   return (
-    <div>
+    <div className={css.container}>
       <Profile
         username={user.username}
         tag={user.tag}
